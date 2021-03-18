054389540bbb054d3bb4c103a55e4305b9672890
angnesegidios <80479161+angnesegidios@users.noreply.github.com>
2021-03-18 20:04:20 -0300

    aula01

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// usando vários comandos no js
+alert("Meu primeiro js");
+// criando variaveis
+var nome = "FULANO DE TAL"
+alert("Bem vindo " + nome)
+/*
+var nome = "FULANO DE TAL"
+var é uma variavel
+nome é uma string
+
+alert("Bem vindo " + nome)
++ é concatenação, que junta duas strings
+*/
