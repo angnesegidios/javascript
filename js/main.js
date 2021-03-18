4bc68baaf7f39d913ee04d4f87404822feb21340
angnesegidios <80479161+angnesegidios@users.noreply.github.com>
2021-03-18 20:44:26 -0300

    Alterações durante a aula

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-// usando vários comandos no js
+/*
+usando vários comandos no js
 alert("Meu primeiro js");
-// criando variaveis
+ criando variaveis
 var nome = "FULANO DE TAL"
 alert("Bem vindo " + nome)
-/*
+
 var nome = "FULANO DE TAL"
 var é uma variavel
 nome é uma string
@@ -11,3 +12,9 @@ nome é uma string
 alert("Bem vindo " + nome)
 + é concatenação, que junta duas strings
 */
+var n1 = 5;
+var n2 = 3;
+var frase = "Japão é o melhor time do mundo";
+console.log(n1 * n2);
+console.log(frase.replace("Japão","Brasil"));
+alert(frase.replace("Japão","Brasil"));
