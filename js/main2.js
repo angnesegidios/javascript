ddac1192cbebf3f96a1bfd95a72c46880ef23e40
angnesegidios <80479161+angnesegidios@users.noreply.github.com>
2021-03-19 11:08:52 -0300

    Alterações durante a aula
    aulas 2 e 3

---
+++ js/main2.js
@@ -0,0 +1,35 @@
+/* AULA 02  Array e Dicionario
+*/ 
+
+/* --- Array --- */
+
+//var lista = ["maçã","pêra","laranja"];
+//lista.push ("uva");
+// push adiciona novo elemento (elemento=array).
+//lista.pop ()
+// pop retira o ultimo elemento.
+//console.log(lista);
+//console.log(lista.length)
+// length mostra a quantidade de elementos citados. No caso são 4 elementos.
+//console.log(lista.reverse());
+// reverse mostra os elementos invertidos, "do final pro começo".
+//console.log(lista.toString());
+// toString perde a referencia de array e torna-se string.
+//console.log(lista[0]);
+// [0] mostra o primeirvar fruta = {nome:"maçã", cor:"vermelha"};
+//console.log(fruta);
+// alert(fruta.nome);
+
+
+/* --- Lista de Dicionario ---
+
+var fruta = {nome:"maçã", cor:"vermelha"};
+console.log(fruta);
+alert(fruta.nome);
+alert(fruta.cor)
+*/
+
+var frutas = [{nome:"maçã", cor:"vermelha"},{nome:"uva", cor:"roxa"}];
+//usa-se conchetes no conjunto de elementos.
+console.log(frutas);
+alert(frutas[1].nome);
---
+++ js/main3.js
@@ -0,0 +1,38 @@
+/* --- Condicionais --- */
+/*
+var idade = prompt("Qual sua idade? ");
+//prompt mostra uma popup de pergunta, colocando a resposta que fica guardada na idade.
+if (idade >= 18){
+    alert("Maior de 18");
+
+}else {(idade < 18)
+    alert("Menor de 18");
+}*/
+
+/* --- Laços de Repetição --- */
+
+/*var count = 0;
+while (count <= 5){
+    console.log(count)
+    count++;
+};*/
+
+/* é uma estrutura de repetição. Conta do 0+1 até chegar <=5. E console.log exibe o resultado no console.
+ count++ adiciona +1 na contagem. Recebe count+1
+*/
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+}
+*/
+
+/* --- Date --- */
+
+var d = new Date();
+alert(d);
+/*alert(d.getMonth()+1);
+alert(d.getDate());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
